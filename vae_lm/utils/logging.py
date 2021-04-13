c3f7a71a38bb84dfe9cfb90f5ae1757710415db8
nemexur <alex.milogradsky@gmail.com>
2021-04-13 21:25:49 +0300

    Slight improvements + bug fixes

--- vae_lm/utils/logging.py
+++ vae_lm/utils/logging.py
@@ -8,6 +8,16 @@ from datetime import datetime
 from rich.console import Console
 
 
+class RichExceptionHandler(logging.Handler):
+    """Much better Rich handler which works with Loguru."""
+    def __init__(self, level: Union[int, str] = logging.NOTSET) -> None:
+        super().__init__(level=level)
+        self._console = Console()
+
+    def emit(self, record: logging.LogRecord) -> None:
+        self._console.print_exception()
+
+
 class SaveBatchHandler(logging.Handler):
     """Handler to save batches with error during training."""
     def __init__(self, level: Union[int, str] = logging.NOTSET) -> None:
@@ -17,19 +27,15 @@ class SaveBatchHandler(logging.Handler):
             self._directory.mkdir(exist_ok=False)
 
     def emit(self, record: logging.LogRecord) -> None:
+        # Construct directory to save batch with error
+        # We need it for better hierarchy
+        save_directory = self._directory / record.extra.get("serialization_dir")
+        if not save_directory.exists():
+            save_directory.mkdir(exist_ok=False)
+        # Use current time as an identifier of an error batch
         time = datetime.now()
         file_suffix = time.strftime("%d-%m-%Y_%H-%M")
-        torch.save(record["batch"], self._directory / f"error-batch-{file_suffix}.pt")
-
-
-class RichExceptionHandler(logging.Handler):
-    """Much better Rich handler which works with Loguru."""
-    def __init__(self, level: Union[int, str] = logging.NOTSET) -> None:
-        super().__init__(level=level)
-        self._console = Console()
-
-    def emit(self, record: logging.LogRecord) -> None:
-        self._console.print_exception()
+        torch.save(record.extra.get("batch"), save_directory / f"batch_{file_suffix}.pt")
 
 
 def setup_logging() -> None:
