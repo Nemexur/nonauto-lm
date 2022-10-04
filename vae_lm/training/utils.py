f2862d96f41ccff35ecd13412d5379f3eab323e3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 16:25:02 +0000

    Adding numeric_owner as keyword arguement

--- vae_lm/training/utils.py
+++ vae_lm/training/utils.py
@@ -168,7 +168,7 @@ def extracted_archive(resolved_archive_file, cleanup=True):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(archive, tempdir)
